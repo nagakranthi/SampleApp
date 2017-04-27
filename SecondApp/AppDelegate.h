1bb1cb7ef09c584863d368d6ed6d5f9bff686804
MAC <mac@macs-macbook-pro.local>
2017-04-27 06:50:36 +0530

    Initial Commit

---
+++ SecondApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondApp
+//
+//  Created by MAC on 4/27/17.
+//  Copyright © 2017 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondApp
+//
+//  Created by MAC on 4/27/17.
+//  Copyright © 2017 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
