a3d4644b9f739c8cf5eecc3ba361f1bdd64fe20f
MAC <mac@macs-macbook-pro.local>
2017-04-27 06:53:58 +0530

    Second view created

---
+++ SecondApp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SecondApp
+//
+//  Created by MAC on 4/27/17.
+//  Copyright © 2017 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
